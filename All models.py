b3eb330392c3dcf2fd16e85f964f7c401ba521ef
MJazizi <azizim@usc.edu>
2019-08-05 16:02:01 -0700

    hyp figed

--- All models.py
+++ All models.py
@@ -30,7 +30,7 @@ from random import sample
 from pybrain.datasets import SupervisedDataSet
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure.modules import SoftmaxLayer
+from pybrain.structure.modules import SoftmaxLayer, TanhLayer, SigmoidLayer, LSTMLayer, LinearLayer
 from sklearn import preprocessing
 
 print("Libs good")
@@ -78,8 +78,8 @@ def convertLabels(label_list):
 
 if __name__ == "__main__":
 
-    # model_name = 'nn'
-    model_name = 'svm'
+    model_name = 'nn'
+    # model_name = 'svm'
     random_state = 12
 
     image_folder = "F:/Acad/research/fafar/RSO/nd_code/alderley/images/"
@@ -87,19 +87,30 @@ if __name__ == "__main__":
         image_folder = sys.argv[1]
 
     # FRAMESA 16960, FRAMESB 14607
-    sample_sizes = [100, 50]
+    sample_sizes = [10, 20]
 
     SHAPE = (30, 30)
-    out_dim = 100
+    # HYP
+    hidden_dim = 100
+    bias_ = True
+    # SoftmaxLayer, TanhLayer, SigmoidLayer, LSTMLayer, LinearLayer
+    hiddenclass_ = TanhLayer
+    outclass_ = SoftmaxLayer
+    # HYP
 
     # generating two numpy arrays for features and labels
     features, labels, num_classes = read_files(image_folder, sample_sizes, model_name)
     # Splitting the data into test and training splits
     X_train, X_test, y_train, y_test = train_test_split(features, labels,
                                                         test_size=0.2, random_state=random_state)
+
+    f = open("result_hd_" + str(hidden_dim) +
+             "b_"+str(bias_) + ".txt", "a")
+    f.write(model_name+"\n")
+
     if model_name == 'nn':
-        net = buildNetwork(SHAPE[0] * SHAPE[1] * 3, out_dim,
-                           num_classes, bias=True, outclass=SoftmaxLayer)
+        net = buildNetwork(SHAPE[0] * SHAPE[1] * 3, hidden_dim,
+                           num_classes, bias=bias_,hiddenclass=hiddenclass_, outclass=outclass_)
 
         train_ds = SupervisedDataSet(SHAPE[0] * SHAPE[1] * 3, num_classes)
         test_ds = SupervisedDataSet(SHAPE[0] * SHAPE[1] * 3, num_classes)
@@ -112,16 +123,20 @@ if __name__ == "__main__":
 
         # checking for model
         if os.path.isfile(model_name+ ".pkl"):
-            print("Using previous model...")
+            tmp = "Using previous "+model_name+ " model...\n"
+            print(tmp)
+            f.write(tmp)
             trainer = pickle.load(open(model_name+ ".pkl", "rb"))
         else:
-            print("Training")
+            tmp = "Training " + model_name+"\n"
+            print(tmp)
+            f.write(tmp)
             trainer = BackpropTrainer(net, train_ds, momentum=0.1,
                                       verbose=True, weightdecay=0.01)
             trainer.train()
 
-            print("Saving model...")
-            pickle.dump(trainer, open(model_name+ ".pkl", "wb"))
+            # print("Saving model")
+            # pickle.dump(trainer, open(model_name+ ".pkl", "wb"))
     elif model_name == 'svm':
         # checking for model
         if os.path.isfile(model_name+ ".pkl"):
@@ -137,7 +152,9 @@ if __name__ == "__main__":
             pickle.dump(svm, open(model_name+ ".pkl", "wb"))
 
     # Test
-    print("Testing...\n")
+    tmp = "Testing...\n"
+    print(tmp)
+    f.write(tmp)
     correct_count = 0
     total_count = 0
     for feature, label in zip(X_test, y_test):
@@ -151,5 +168,7 @@ if __name__ == "__main__":
             correct_count += 1
         total_count += 1
     acc = (float(correct_count) / float(total_count)) * 100
-
-    print('acc is {}'.format(acc))
+    tmp = 'acc is {} \n'.format(acc)
+    print(tmp)
+    f.write(tmp)
+    f.close()
